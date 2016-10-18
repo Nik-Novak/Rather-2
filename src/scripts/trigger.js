21c5797904c2910b562dea9bdeed799cb8f96f42
vareye <vareye@mcmaster.ca>
2016-10-17 22:29:32 -0400

    function that finds trigger words

--- src/scripts/trigger.js
+++ src/scripts/trigger.js
@@ -1,18 +1,40 @@
 function trigger(word){
-<script type="text/javascript"/>
-	var reader = new FileReader();
-	var textFile = input.files[0]
-	reader.readasText(textFile);
-	$(reader).on('load', processFile);
-function processFile(f){
-	var file = f.target.result,
-		results;
-	if(file && file.length){
-		line = file.split("\n");
-		var catch = line.search(trigger);
-}
-if(catch.length > 0){
-windows.alert("Word Found");
+var fileInput = $('#files');
+var uploadButton = $('#upload');
 
+uploadButton.on('click', function() {
+    if (!window.FileReader) {
+        alert('Your browser is not supported');
+        return false;
+    }
+    var input = fileInput.get(0);
+
+    // Create a reader object
+    var reader = new FileReader();
+    if (input.files.length) {
+        var textFile = input.files[0];
+        // Read the file
+        reader.readAsText(textFile);
+        // When it's loaded, process it
+        $(reader).on('load', processFile);
+    } else {
+        alert('Please upload a file before continuing')
+    } 
+});
+
+function processFile(e) {
+    var file = e.target.result,
+        results;
+    if (file && file.length) {
+        results = file.split("\n");
+	for(i=0; i<results.length ; i++){
+		if(results[i] == word){
+			results[i] = "Trigger word found";
+        		$('#name').val(results[i]);
+        		$('#age').val(results[i]);
+    
+		}
+	}
+   }
 }
 }
