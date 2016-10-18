8abe43acb7cd11dfe4ee5344868f31652f76334b
vareye <vareye@mcmaster.ca>
2016-10-17 21:57:30 -0400

    Search function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,32 +2,26 @@ var myHeading =  document.querySelector('h1');
 myHeading.textContent = 'rather i guess?';
 
 document.write(
-  "Okay now that we have the basics and shit<br>");
+  "Okay now that we have the basics<br>");
 document.write(
   "I AM A GOD OF CODING"
 )
 
-
-
-/*<input type="file" id="fileinput"/>
+function trigger(word){
 <script type="text/javascript"/>
-  function readSingleFile(evt){
-    var f = evt.target.files[0];
-
-    if (f){
-      var r = new FileReader();
-      r.onload = function(e) {
-        var contents =  e.target.result;
-        alert( "Got the file.n"
-        + "name: " + f.name + "\n"
-        + "type: " + f.type + "\n"
-        + "size: " + f.size + " bytes\n")
-        + "starts with: " + contents.substr(1, contents.indexOf("n"))
-      );
-      }
-      r.readAsText(f);
-    } else {
-      alert("Failed to load file.");
-    }
-  }
-*/
+	var reader = new FileREader();
+	var textFile = input.files[0]
+	reader.readasText(textFile);
+	$(reader).on('load', processFile);
+function processFile(f){
+	var file = f.target.result,
+		results;
+	if(file && file.length){
+		var line = file.split("\n");
+		var catch = line.search(trigger);
+}
+if(catch.length > 0){
+windows.alert("Word Found");
+		
+}
+}
