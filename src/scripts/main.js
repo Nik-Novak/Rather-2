91e9e258bd53dcc935c8cf69d4e304de5d2ee48e
straatjc <straatjc@mcmaster.ca>
2016-10-17 21:10:00 -0400

    Adding html and js files to test on

---
+++ src/scripts/main.js
@@ -0,0 +1,33 @@
+var myHeading =  document.querySelector('h1');
+myHeading.textContent = 'rather i guess?';
+
+document.write(
+  "Okay now that we have the basics and shit<br>");
+document.write(
+  "I AM A GOD OF CODING"
+)
+
+
+
+/*<input type="file" id="fileinput"/>
+<script type="text/javascript"/>
+  function readSingleFile(evt){
+    var f = evt.target.files[0];
+
+    if (f){
+      var r = new FileReader();
+      r.onload = function(e) {
+        var contents =  e.target.result;
+        alert( "Got the file.n"
+        + "name: " + f.name + "\n"
+        + "type: " + f.type + "\n"
+        + "size: " + f.size + " bytes\n")
+        + "starts with: " + contents.substr(1, contents.indexOf("n"))
+      );
+      }
+      r.readAsText(f);
+    } else {
+      alert("Failed to load file.");
+    }
+  }
+*/
